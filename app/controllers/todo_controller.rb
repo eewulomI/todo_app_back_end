2eec59171d50b6c1ade503de11f67016de60987f
Ezekiel Ewulomi <zalante26@gmail.com>
2017-07-21 13:26:05 +0000

    Install postgres and update database.yml

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,22 @@
 class TodoController < ApplicationController
     def index
     end
+    def show
+    
+    if params["id"] == 1
+    @todo_description= "buy_water"
+    @todo_pomodoro= 5
+
+    elsif  params["id"] == 1
+    @todo_description= "sleep"
+    @todo_pomodoro= 12
+     
+     elsif  params["id"] == 1
+     @todo_description= "exercise"
+     @todo_pomodoro= 15
+     
+
+
+    end
+    end
 end
